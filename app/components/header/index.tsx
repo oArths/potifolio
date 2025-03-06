860ac949cc07d33fce64cf98227920b7dec9b47f
Arthur <arthurferreira1417@gmail.com>
2025-03-06 15:32:44 -0300

    Feat: adding a animation in header

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,9 +1,35 @@
+"use client";
 import * as I from "lucide-react";
 import Image from "next/image";
+import { useEffect } from "react";
+import { HeaderIcon } from "@/public/images";
 const Header = () => {
+  let lastScrollTop = 0;
+
+  useEffect(() => {
+    const header = document.getElementById("header");
+    if (!header) return;
+    const handleScroll = () => {
+      let scrollTop = window.scrollY || document.documentElement.scrollTop;
+      scrollTop > lastScrollTop ? header.style.top = "-80px" : header.style.top = "0";
+      lastScrollTop = scrollTop;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
-    <header className="text-50 flex flex-row items-center justify-start px-9 border-2 bg-950/30  backdrop-blur-md  z-50 top-0 fixed w-full h-16 border-950 border-b-400  border-b-[0.5px]  ">
-      <Image src="./images/favicon.svg" alt="Logo" width={50} height={50} />
+    <header
+      id="header"
+      className="text-50 flex flex-row items-center justify-start px-9 border-2 bg-950/30  backdrop-blur-md  z-50 top-0 fixed w-full h-16 border-950 border-b-400  border-b-[0.5px]  "
+    >
+      <div className=" flex items-center justify-center w-[50] h-[50]">
+        <Image
+          src={HeaderIcon}
+          alt="Logo"
+          className="object-contain w-[50px] h-auto"
+        />
+      </div>
       <nav className="text-50 flex  w-auto mx-auto flex-row items-center justify-between ">
         <ul className="flex items-center max-md:hidden gap-28 justify-between">
           <li>
