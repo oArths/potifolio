21733877aba72bb966dc867736a0b7a00753fb23
Arthur Ferreira Nascimento <arthurferreira1417@gmail.com>
2025-11-20 19:54:20 -0300

    Feat(Langue): adding a config for change langue option with zustand

---
+++ app/locales/interface.d.ts
@@ -0,0 +1,9 @@
+export interface TranslationObject {
+  [key: string]: string | TranslationObject
+}
+
+export interface Translations {
+  [key: string]: TranslationObject
+}
+
+export type LanguageCode = "pt" | "en";
---
+++ app/store/langueStore.ts
@@ -0,0 +1,35 @@
+import { create } from "zustand";
+import pt from "../locales/pt.json";
+import en from "../locales/en.json";
+import { Translations, TranslationObject , LanguageCode} from "../locales/interface";
+
+const languages: Translations = { pt, en };
+
+type LangState = {
+  lang: LanguageCode;
+  t: (path: string) => string;
+  setLang: (l: LanguageCode) => void;
+};
+
+const getTranslation = (path: string, translations: TranslationObject): string => {
+  const keys = path.split('.');
+  let current: unknown = translations;
+  
+  for (const key of keys) {
+    if (current && typeof current === 'object' && !Array.isArray(current)) {
+      current = (current as Record<string, unknown>)[key];
+    } else {
+      return path;
+    }
+  }
+  return typeof current === 'string' ? current : path;
+};
+
+export const useLangueStore = create<LangState>((set, get) => ({
+  lang: "pt",
+  t: (path: string) => {
+    const currentTranslations  = languages[get().lang];
+    return getTranslation(path,currentTranslations)
+  },
+  setLang: (newLang: LanguageCode) => set({ lang: newLang }),
+}));
